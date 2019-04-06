4f440fcf40f6fdcab9145e3fb4fcc6e767652f82
Gleb Svechnikov <gleb.svechnikov@simplinic.com>
2019-04-06 22:04:41 +0300

    Plug to lab240

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import MQTT from "mqtt";
 
 class App extends Component {
+  mqttSub = () => {
+    const client = MQTT.connect({
+      port: 9001,
+      host: "ns.lab240.ru",
+      // clientId:
+      //   "ui_" +
+      //   Math.random()
+      //     .toString(16)
+      //     .substr(2, 8),
+      path: "/mqtt",
+      protocol: "ws",
+      username: "gleb",
+      password: "12344321"
+    });
+
+    client.on("connect", () => {
+      console.log("connected");
+      client.subscribe("/gleb/dev1/out/time_up", { qos: 1 }, error => {
+        // console.error(error);
+        if (!error) {
+          console.log("subscribed");
+        }
+      });
+      client.on("message", (topic, message) => {
+        console.log("topic", topic, message);
+      });
+    });
+  };
+  componentDidMount() {
+    this.mqttSub();
+  }
   render() {
     return (
       <div className="App">
