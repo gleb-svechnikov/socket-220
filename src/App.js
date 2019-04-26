809cba3a781519ec702a84e4da0781ea7d61f2d3
Gleb Svechnikov <gleb.svechnikov@simplinic.com>
2019-04-26 15:31:49 +0300

    Update form and styles

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ class App extends Component {
     super(props);
     this.state = {
       url: "",
+      open: true,
+      devices: [],
       port: "",
       username: "",
       fulfilled: false,
@@ -15,11 +17,11 @@ class App extends Component {
       uptime: "",
       tempIn: {
         date: new Date(),
-        degrees: null
+        degrees: 0
       },
       tempOut: {
         date: new Date(),
-        degrees: null
+        degrees: 0
       }
     };
   }
@@ -39,40 +41,63 @@ class App extends Component {
     });
 
     client.on("connect", () => {
-      console.log("connected");
-      client.subscribe("/" + user + "/dev1/out/time_up", { qos: 1 }, error => {
-        // console.error(error);
-        if (!error) {
-          console.log("subscribed");
-          console.log(this.state.data);
-        }
-      });
-      client.subscribe("/" + user + "/dev1/out/temp_out", { qos: 1 }, error => {
-        // console.error(error);
-        if (!error) {
-          console.log("subscribed");
-          // console.log(this.state.data);
-        }
-      });
-      client.subscribe("/" + user + "/dev1/out/temp_in", { qos: 1 }, error => {
-        // console.error(error);
-        if (!error) {
-          console.log("subscribed");
-          // console.log(this.state.data);
-        }
-      });
+      console.log("connected", this.state.devices);
+      for (const device of this.state.devices) {
+        client.subscribe(
+          "/" + user + "/" + device + "/out/time_up",
+          { qos: 1 },
+          error => {
+            // console.error(error);
+            if (!error) {
+              console.log("subscribed");
+              console.log(this.state.data);
+            }
+          }
+        );
+        client.subscribe(
+          "/" + user + "/" + device + "/out/temp_out",
+          { qos: 1 },
+          error => {
+            // console.error(error);
+            if (!error) {
+              console.log("subscribed");
+              // console.log(this.state.data);
+            }
+          }
+        );
+        client.subscribe(
+          "/" + user + "/" + device + "/out/temp_in",
+          { qos: 1 },
+          error => {
+            // console.error(error);
+            if (!error) {
+              console.log("subscribed");
+              // console.log(this.state.data);
+            }
+          }
+        );
+      }
+
       client.on("message", (topic, message) => {
         console.log(topic, this.uintToString(message));
-
-        if (topic === "/" + user + "/dev1/out/time_up") {
+        if (
+          topic ===
+          "/" + user + "/" + this.state.devices[0] + "/out/time_up"
+        ) {
           this.setState({ uptime: this.uintToString(message) });
         }
-        if (topic === "/" + user + "/dev1/out/temp_out") {
+        if (
+          topic ===
+          "/" + user + "/" + this.state.devices[0] + "/out/temp_out"
+        ) {
           this.setState({
             tempOut: { date: new Date(), degrees: this.uintToString(message) }
           });
         }
-        if (topic === "/" + user + "/dev1/out/temp_in") {
+        if (
+          topic ===
+          "/" + user + "/" + this.state.devices[0] + "/out/temp_in"
+        ) {
           this.setState({
             tempIn: { date: new Date(), degrees: this.uintToString(message) }
           });
@@ -95,13 +120,21 @@ class App extends Component {
     console.log(event.target.value);
     this.setState({ password: event.target.value });
   };
+  changeDevices = event => {
+    console.log("Devices", event.target.value);
+    this.setState({
+      devices: event.target.value.replace(/\s/g, "").split(",")
+    });
+  };
+
   componentDidMount() {
     setInterval(() => this.forceUpdate(), 6000);
   }
   onSubmit = event => {
     event.preventDefault();
     this.setState({
-      fulfilled: true
+      fulfilled: true,
+      open: false
     });
     console.log(
       this.state.url,
@@ -130,72 +163,95 @@ class App extends Component {
       //     // show: false
       //   }
     };
+    const {
+      open,
+      url,
+      port,
+      fulfilled,
+      username,
+      devices,
+      password,
+      uptime,
+      tempOut,
+      tempIn
+    } = this.state;
+    console.log(tempOut, tempIn);
+
     return (
       <main className="App">
-        <form onSubmit={this.onSubmit}>
-          <fieldset>
-            <label htmlFor="domain">Domain</label>
-            <input
-              type="url"
-              id="domain"
-              value={this.state.url}
-              required
-              onChange={this.changeURL}
-              placeholder="ws://test.net"
-            />
-          </fieldset>
-          <fieldset>
-            <label htmlFor="port">Port</label>
-            <input
-              type="number"
-              id="port"
-              value={this.state.port}
-              required
-              onChange={this.changePort}
-              placeholder="9001"
-            />
-          </fieldset>
-          <fieldset>
-            <label htmlFor="username">Username</label>
-            <input
-              type="text"
-              id="username"
-              value={this.state.username}
-              required
-              onChange={this.changeUsername}
-              placeholder="user1"
-            />
-          </fieldset>
-          <fieldset>
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              value={this.state.password}
-              required
-              onChange={this.changePassword}
-            />
-          </fieldset>
+        <details open={open}>
+          <summary>
+            {!fulfilled ? "Connection credetials" : "Connected"}
+          </summary>
+          <form onSubmit={this.onSubmit}>
+            <fieldset>
+              <label htmlFor="domain">Domain</label>
+              <input
+                type="url"
+                id="domain"
+                value={url}
+                required
+                onChange={this.changeURL}
+                placeholder="ws://test.net"
+              />
+            </fieldset>
+            <fieldset>
+              <label htmlFor="port">Port</label>
+              <input
+                type="number"
+                id="port"
+                value={port}
+                required
+                onChange={this.changePort}
+                placeholder="9001"
+              />
+            </fieldset>
+            <fieldset>
+              <label htmlFor="devices">Devices</label>
+              <input
+                type="text"
+                id="devices"
+                value={devices.toString()}
+                required
+                onChange={this.changeDevices}
+                placeholder="dev1, dev2"
+              />
+            </fieldset>
+            <fieldset>
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                id="username"
+                value={username}
+                required
+                onChange={this.changeUsername}
+                placeholder="user1"
+              />
+            </fieldset>
+            <fieldset>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                required
+                onChange={this.changePassword}
+              />
+            </fieldset>
 
-          <button type="submit">Connect</button>
-        </form>
+            <button type="submit">Connect</button>
+          </form>
+        </details>
+
         {this.state.fulfilled && (
           <>
-            <h2>Uptime {this.state.uptime}</h2>
+            <h2>Uptime {uptime}</h2>
             <hr />
             <h2>Temperature outside ℃</h2>
-            <RTChart
-              chart={chart}
-              fields={["degrees"]}
-              data={this.state.tempOut}
-            />
+            <RTChart chart={chart} fields={["degrees"]} data={tempOut} />
             <hr />
             <h2>Socket temperature ℃</h2>
-            <RTChart
-              chart={chart}
-              fields={["degrees"]}
-              data={this.state.tempIn}
-            />
+            <RTChart chart={chart} fields={["degrees"]} data={tempIn} />
           </>
         )}
       </main>
